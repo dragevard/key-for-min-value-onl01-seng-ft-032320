61e8c32ed2be7bed1b5668f76b0b23fc90421582
Derek phung <dragevards@gmail.com>
2020-03-31 22:16:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,19 @@
 
 def key_for_min_value(name_hash)
   smallest_value = ""
+  name = ""
+  array = []
   key_for_min_value.each do |key, value|
+    array.push(value)
+  end
+  array.sort!
+  smallest_value = array[0]
+  key_for_min_value.each do |key, value|
+    if smallest_value == value
+      name = key
+    end
+  end
+  name
+end
     
     
